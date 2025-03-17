6060f42c4d6a9945397fe5790cb99bd9ea880749
leogytis <leogytis@gmail.com>
2025-03-17 07:53:28 +0200

    save

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
 	output: 'export',
-	distDir: 'out',
-	basePath: '/shift_mind',
+	// distDir: 'out',
+	// basePath: '/shift_mind',
 	// assetPrefix: '/shift_mind',
-	assetPrefix: './',
+	// assetPrefix: './',
 	images: {
 		unoptimized: true,
 	},
