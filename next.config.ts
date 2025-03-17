a3af254760aea14168a72d4856a518474426f996
leogytis <leogytis@gmail.com>
2025-03-17 08:01:53 +0200

    clean up

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
 	output: 'export',
-	// distDir: 'out',
-	// basePath: '/shift_mind',
-	// assetPrefix: '/shift_mind',
-	// assetPrefix: './',
 	images: {
 		unoptimized: true,
 	},
