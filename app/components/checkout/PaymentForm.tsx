897ecd6d6010299fa3e8f35e21b67eb9b9418ad5
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-04-25 15:17:29 +0300

    Add prettier-plugin-tailwindcss and update PaymentForm and CardsSection components

--- app/components/checkout/PaymentForm.tsx
+++ app/components/checkout/PaymentForm.tsx
@@ -1,168 +1,228 @@
-import {creditCardSchema} from '@/app/utils/validationSchemas';
-import apple from '@/app/vectors/apple.svg';
-import creditcardpink from '@/app/vectors/creditcardpink.svg';
-import creditcards from '@/app/vectors/creditcards.svg';
-import discover from '@/app/vectors/discover.svg';
-import mastercardblue from '@/app/vectors/mastercardblue.svg';
-import mastercardyellow from '@/app/vectors/mastercardyellow.svg';
-import paypal from '@/app/vectors/paypal.svg';
-import subtract from '@/app/vectors/subtract.svg';
-import visa from '@/app/vectors/visa.svg';
-import {zodResolver} from '@hookform/resolvers/zod';
-import Image from 'next/image';
-import {useState} from 'react';
-import {useForm} from 'react-hook-form';
-import FormInput from '../FormInput';
-import {dummyCardData} from '../choose-plan-section/dummyCardData';
+import { creditCardSchema } from "@/app/utils/validationSchemas";
+import apple from "@/app/vectors/apple.svg";
+import creditcardpink from "@/app/vectors/creditcardpink.svg";
+import creditcards from "@/app/vectors/creditcards.svg";
+import discover from "@/app/vectors/discover.svg";
+import mastercardblue from "@/app/vectors/mastercardblue.svg";
+import mastercardyellow from "@/app/vectors/mastercardyellow.svg";
+import paypal from "@/app/vectors/paypal.svg";
+import subtract from "@/app/vectors/subtract.svg";
+import visa from "@/app/vectors/visa.svg";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import FormInput from "../FormInput";
+import { dummyCardData } from "../choose-plan-section/dummyCardData";
 
 interface FormValues {
-	cardNumber: string;
-	nameOnCard: string;
-	expiryDate: string;
-	cvv: string;
+  cardNumber: string;
+  nameOnCard: string;
+  expiryDate: string;
+  cvv: string;
 }
 
 interface PaymentFormProps {
-	selectedPlan: number;
-	onNextStep: () => void;
+  selectedPlan: number;
+  onNextStep: () => void;
 }
 
-const PaymentForm: React.FC<PaymentFormProps> = ({selectedPlan, onNextStep}) => {
-	const [submitError, setSubmitError] = useState(false);
-	const plan = dummyCardData[selectedPlan];
+const PaymentForm: React.FC<PaymentFormProps> = ({
+  selectedPlan,
+  onNextStep,
+}) => {
+  const [submitError, setSubmitError] = useState(false);
+  const plan = dummyCardData[selectedPlan];
 
-	const {control, handleSubmit} = useForm<FormValues>({
-		resolver: zodResolver(creditCardSchema),
-		defaultValues: {
-			cardNumber: '',
-			expiryDate: '',
-			cvv: '',
-			nameOnCard: '',
-		},
-	});
+  const { control, handleSubmit } = useForm<FormValues>({
+    resolver: zodResolver(creditCardSchema),
+    defaultValues: {
+      cardNumber: "",
+      expiryDate: "",
+      cvv: "",
+      nameOnCard: "",
+    },
+  });
 
-	const onSubmit = (data: FormValues) => {
-		const simulateError = false;
-		if (simulateError) {
-			setSubmitError(true);
-		} else {
-			alert('Form Submitted!');
-			console.log(data);
-			onNextStep();
-		}
-	};
+  const onSubmit = (data: FormValues) => {
+    const simulateError = false;
+    if (simulateError) {
+      setSubmitError(true);
+    } else {
+      alert("Form Submitted!");
+      console.log(data);
+      onNextStep();
+    }
+  };
 
-	const handleCardNumberChange = (e: React.ChangeEvent<HTMLInputElement>, onChange: (value: string) => void) => {
-		const value = e.target.value.replace(/\D/g, '').slice(0, 16);
-		const formattedValue = value.replace(/(\d{4})(?=\d)/g, '$1 ');
-		onChange(formattedValue);
-	};
+  const handleCardNumberChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    onChange: (value: string) => void,
+  ) => {
+    const value = e.target.value.replace(/\D/g, "").slice(0, 16);
+    const formattedValue = value.replace(/(\d{4})(?=\d)/g, "$1 ");
+    onChange(formattedValue);
+  };
 
-	const handleExpiryDateChange = (e: React.ChangeEvent<HTMLInputElement>, onChange: (value: string) => void) => {
-		const value = e.target.value.replace(/\D/g, '').slice(0, 4);
-		const formattedValue = value.replace(/(\d{2})(?=\d)/g, '$1/');
-		onChange(formattedValue);
-	};
+  const handleExpiryDateChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    onChange: (value: string) => void,
+  ) => {
+    const value = e.target.value.replace(/\D/g, "").slice(0, 4);
+    const formattedValue = value.replace(/(\d{2})(?=\d)/g, "$1/");
+    onChange(formattedValue);
+  };
 
-	const handleCvvChange = (e: React.ChangeEvent<HTMLInputElement>, onChange: (value: string) => void) => {
-		const value = e.target.value.replace(/\D/g, '').slice(0, 3);
-		onChange(value);
-	};
+  const handleCvvChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    onChange: (value: string) => void,
+  ) => {
+    const value = e.target.value.replace(/\D/g, "").slice(0, 3);
+    onChange(value);
+  };
 
-	return (
-		<>
-			<h3 className="mb-4">Order Summary</h3>
-			<div className="flex flex-col gap-4 border border-green-lightest rounded-2xl text-greendark p-4 mb-[43px]">
-				<div className="flex justify-between items-center border-b border-green-lightest pb-4">
-					<span className="text-sm">{plan.title}</span>
-					<span className="font-semibold">${plan.originalPrice}</span>
-				</div>
-				<div className="flex justify-between items-center border-b border-green-lightest text-pink text-sm pb-4">
-					<span className="text-sm">Discount (50%)</span>
-					<span className="font-semibold">-${plan.discountedPrice}</span>
-				</div>
-				<div className="flex justify-between items-center">
-					<span className="font-semibold">Total</span>
-					<span className="text-2xl font-semibold">${plan.discountedPrice}</span>
-				</div>
-			</div>
+  return (
+    <>
+      <h3 className="mb-4">Order Summary</h3>
+      <div className="border-green-lightest text-greendark mb-[43px] flex flex-col gap-4 rounded-2xl border p-4">
+        <div className="border-green-lightest flex items-center justify-between border-b pb-4">
+          <span className="text-sm">{plan.title}</span>
+          <span className="font-semibold">${plan.originalPrice}</span>
+        </div>
+        <div className="border-green-lightest text-pink flex items-center justify-between border-b pb-4 text-sm">
+          <span className="text-sm">Discount (50%)</span>
+          <span className="font-semibold">-${plan.discountedPrice}</span>
+        </div>
+        <div className="flex items-center justify-between">
+          <span className="font-semibold">Total</span>
+          <span className="text-2xl font-semibold">
+            ${plan.discountedPrice}
+          </span>
+        </div>
+      </div>
 
-			<h3 className="mb-5">Select Payment Method</h3>
-			<div className="flex gap-[11px]">
-				<button className="w-1/2 flex justify-center items-center bg-bgyellow rounded-3xl cursor-pointer py-5">
-					<Image src={paypal} alt="paypal_icon" layout="intrinsic" className="object-contain" />
-				</button>
-				<button className="w-1/2 flex justify-center items-center bg-black rounded-3xl cursor-pointer py-5">
-					<Image src={apple} alt="apple_icon" layout="intrinsic" className="object-contain" />
-				</button>
-			</div>
+      <h3 className="mb-5">Select Payment Method</h3>
+      <div className="flex gap-[11px]">
+        <button className="bg-bgyellow flex w-1/2 cursor-pointer items-center justify-center rounded-3xl py-5">
+          <Image
+            src={paypal}
+            alt="paypal_icon"
+            layout="intrinsic"
+            className="object-contain"
+          />
+        </button>
+        <button className="flex w-1/2 cursor-pointer items-center justify-center rounded-3xl bg-black py-5">
+          <Image
+            src={apple}
+            alt="apple_icon"
+            layout="intrinsic"
+            className="object-contain"
+          />
+        </button>
+      </div>
 
-			<div className="flex items-center my-5">
-				<div className="flex-grow border-t border-greendark"></div>
-				<span className="text-greendark text-[17px] font-bold mx-3">or</span>
-				<div className="flex-grow border-t border-greendark"></div>
-			</div>
+      <div className="my-5 flex items-center">
+        <div className="border-greendark flex-grow border-t"></div>
+        <span className="text-greendark mx-3 text-[17px] font-bold">or</span>
+        <div className="border-greendark flex-grow border-t"></div>
+      </div>
 
-			{/* ----- Form ----- */}
-			<form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3.5 mb-8">
-				<div className="flex justify-between items-center">
-					<h3>Credit Card</h3>
-					<Image src={creditcards} alt="creditcards_icon" layout="intrinsic" className="object-contain" />
-				</div>
-				<div className="relative">
-					<Image
-						src={creditcardpink}
-						alt="creditcardpink_icon"
-						layout="intrinsic"
-						className="object-contain absolute left-2 top-4.5"
-					/>
-					<FormInput
-						name="cardNumber"
-						placeholder="XXXX XXXX XXXX XXXX"
-						onChange={(e, fieldOnChange) => handleCardNumberChange(e, fieldOnChange)}
-						control={control}
-						className="!pl-10"
-					/>
-				</div>
-				<FormInput name="nameOnCard" placeholder="Name on card" control={control} />
-				<div className="w-full flex gap-4">
-					<FormInput
-						name="expiryDate"
-						placeholder="MM/YY"
-						onChange={(e, fieldOnChange) => handleExpiryDateChange(e, fieldOnChange)}
-						control={control}
-					/>
-					<FormInput
-						name="cvv"
-						placeholder="CVV/CVC"
-						control={control}
-						onChange={(e, fieldOnChange) => handleCvvChange(e, fieldOnChange)}
-					/>
-				</div>
-				<button className="w-full background-gradient rounded-3xl text-white font-semibold cursor-pointer py-6 mt-4">
-					SUBMIT SECURE ORDER
-				</button>
-				{submitError && (
-					<p className="flex gap-2 self-center text-pink text-sm">
-						<Image src={subtract} alt="exclamation_icon" layout="intrinsic" className="object-contain" />
-						Oops, payment failed! Please try again.
-					</p>
-				)}
-			</form>
+      {/* ----- Form ----- */}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mb-8 flex flex-col gap-3.5"
+      >
+        <div className="flex items-center justify-between">
+          <h3>Credit Card</h3>
+          <Image
+            src={creditcards}
+            alt="creditcards_icon"
+            layout="intrinsic"
+            className="object-contain"
+          />
+        </div>
+        <div className="relative">
+          <Image
+            src={creditcardpink}
+            alt="creditcardpink_icon"
+            layout="intrinsic"
+            className="absolute top-4.5 left-2 object-contain"
+          />
+          <FormInput
+            name="cardNumber"
+            placeholder="XXXX XXXX XXXX XXXX"
+            onChange={(e, fieldOnChange) =>
+              handleCardNumberChange(e, fieldOnChange)
+            }
+            control={control}
+            className="!pl-10"
+          />
+        </div>
+        <FormInput
+          name="nameOnCard"
+          placeholder="Name on card"
+          control={control}
+        />
+        <div className="flex w-full gap-4">
+          <FormInput
+            name="expiryDate"
+            placeholder="MM/YY"
+            onChange={(e, fieldOnChange) =>
+              handleExpiryDateChange(e, fieldOnChange)
+            }
+            control={control}
+          />
+          <FormInput
+            name="cvv"
+            placeholder="CVV/CVC"
+            control={control}
+            onChange={(e, fieldOnChange) => handleCvvChange(e, fieldOnChange)}
+          />
+        </div>
+        <button className="background-gradient mt-4 w-full cursor-pointer rounded-3xl py-6 font-semibold text-white">
+          SUBMIT SECURE ORDER
+        </button>
+        {submitError && (
+          <p className="text-pink flex gap-2 self-center text-sm">
+            <Image
+              src={subtract}
+              alt="exclamation_icon"
+              layout="intrinsic"
+              className="object-contain"
+            />
+            Oops, payment failed! Please try again.
+          </p>
+        )}
+      </form>
 
-			<div className="flex justify-center gap-3.5">
-				<Image src={visa} alt="visa_icon" layout="intrinsic" className="object-contain" />
-				<Image
-					src={mastercardyellow}
-					alt="mastercardyellow_icon"
-					layout="intrinsic"
-					className="object-contain"
-				/>
-				<Image src={discover} alt="discover_icon" layout="intrinsic" className="object-contain" />
-				<Image src={mastercardblue} alt="mastercardblue_icon" layout="intrinsic" className="object-contain" />
-			</div>
-		</>
-	);
+      <div className="flex justify-center gap-3.5">
+        <Image
+          src={visa}
+          alt="visa_icon"
+          layout="intrinsic"
+          className="object-contain"
+        />
+        <Image
+          src={mastercardyellow}
+          alt="mastercardyellow_icon"
+          layout="intrinsic"
+          className="object-contain"
+        />
+        <Image
+          src={discover}
+          alt="discover_icon"
+          layout="intrinsic"
+          className="object-contain"
+        />
+        <Image
+          src={mastercardblue}
+          alt="mastercardblue_icon"
+          layout="intrinsic"
+          className="object-contain"
+        />
+      </div>
+    </>
+  );
 };
 
 export default PaymentForm;
--- app/components/choose-plan-section/CardsSection.tsx
+++ app/components/choose-plan-section/CardsSection.tsx
@@ -1,112 +1,107 @@
-import arrows from '@/app/vectors/arrows.svg';
-import Image from 'next/image';
-import {FC} from 'react';
+import arrows from "@/app/vectors/arrows.svg";
+import Image from "next/image";
+import { FC } from "react";
 
 interface CardProps {
-	image: {
-		src: string;
-		alt: string;
-	};
-	colorCode: string;
-	title: string;
-	list: string[];
+  image: {
+    src: string;
+    alt: string;
+  };
+  colorCode: string;
+  title: string;
+  list: string[];
 }
 
-const Card: FC<CardProps> = ({image, colorCode, title, list}) => {
-	return (
-		<div className="mb-12 lg:p-0 hover:bg-white rounded-3xl card-shadow hover-card-shadow group transition-all duration-300 w-full md:w-[410px]">
-			<div className="flex flex-col gap-6 w-full">
-				<div className="h-full overflow-hidden rounded-t-3xl">
-					<Image
-						src={image.src}
-						alt={image.alt}
-						width={400}
-						height={293}
-						className="w-full h-[293px] object-cover hover:scale-110 ease-out duration-700 transform"
-					/>
-				</div>
-				<div className="flex flex-col p-6">
-					<h2
-						style={{color: colorCode}}
-						className="text-2xl font-bold mb-8"
-					>
-						{title}
-					</h2>
-					<ul className="space-y-5">
-						{list.map((item, index) => (
-							<li
-								key={index}
-								className="flex items-center gap-4 group-hover:translate-x-2 transition-transform duration-300"
-							>
-								<span
-									style={{backgroundColor: colorCode}}
-									className="w-2.5 h-2.5 rounded-full"
-								></span>
-								<span className="text-gray-700 text-lg">
-									{item}
-								</span>
-							</li>
-						))}
-					</ul>
-				</div>
-			</div>
-		</div>
-	);
+const Card: FC<CardProps> = ({ image, colorCode, title, list }) => {
+  return (
+    <div className="card-shadow hover-card-shadow group mb-12 w-full rounded-3xl transition-all duration-300 hover:bg-white md:w-[410px] lg:p-0">
+      <div className="flex w-full flex-col gap-6">
+        <div className="h-full overflow-hidden rounded-t-3xl">
+          <Image
+            src={image.src}
+            alt={image.alt}
+            width={400}
+            height={293}
+            className="h-[293px] w-full transform object-cover duration-700 ease-out hover:scale-110"
+          />
+        </div>
+        <div className="flex flex-col p-6">
+          <h2 style={{ color: colorCode }} className="mb-8 text-2xl font-bold">
+            {title}
+          </h2>
+          <ul className="space-y-5">
+            {list.map((item, index) => (
+              <li
+                key={index}
+                className="flex items-center gap-4 transition-transform duration-300 group-hover:translate-x-2"
+              >
+                <span
+                  style={{ backgroundColor: colorCode }}
+                  className="h-2.5 w-2.5 rounded-full"
+                ></span>
+                <span className="text-lg text-gray-700">{item}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const CardOne: CardProps = {
-	image: {
-		src: '/images/procrastination.jpg',
-		alt: 'Procrastination Symptoms',
-	},
-	colorCode: '#ff4d8f',
-	title: 'PROCRASTINATION SYMPTOMS:',
-	list: [
-		'Time management difficulties',
-		'Poor organizational skills',
-		'Difficulty focusing',
-		'Acting without thinking',
-		'Restlessness',
-	],
+  image: {
+    src: "/images/procrastination.jpg",
+    alt: "Procrastination Symptoms",
+  },
+  colorCode: "#ff4d8f",
+  title: "PROCRASTINATION SYMPTOMS:",
+  list: [
+    "Time management difficulties",
+    "Poor organizational skills",
+    "Difficulty focusing",
+    "Acting without thinking",
+    "Restlessness",
+  ],
 };
 
 const CardTwo: CardProps = {
-	image: {
-		src: '/images/your_goals.jpg',
-		alt: 'Your Goals',
-	},
-	colorCode: '#5a91ff',
-	title: 'YOUR GOAL:',
-	list: [
-		'Able to focus, minimal distraction',
-		'Effective planning, punctuality',
-		'Tidy spaces, structured routines',
-		'Considerate actions, patience',
-		'Calmness',
-	],
+  image: {
+    src: "/images/your_goals.jpg",
+    alt: "Your Goals",
+  },
+  colorCode: "#5a91ff",
+  title: "YOUR GOAL:",
+  list: [
+    "Able to focus, minimal distraction",
+    "Effective planning, punctuality",
+    "Tidy spaces, structured routines",
+    "Considerate actions, patience",
+    "Calmness",
+  ],
 };
 
 const CardsSection: FC = () => {
-	return (
-		<section className="w-full py-16 px-4 md:px-8 bg-[#f8fafc]">
-			<div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-stretch gap-8 md:gap-16">
-				<Card {...CardOne} />
+  return (
+    <section className="w-full bg-[#f8fafc] px-4 py-16 md:px-8">
+      <div className="mx-auto flex max-w-6xl flex-col items-stretch justify-between gap-8 md:flex-row md:gap-16">
+        <Card {...CardOne} />
 
-				{/* Arrow Section */}
-				<div className="flex md:flex-col items-center justify-center gap-3">
-					<Image
-						src={arrows}
-						alt="arrows"
-						width={50}
-						height={50}
-						className="w-full h-[293px] object-cover hover:scale-110 ease-out duration-700 transform"
-					/>
-				</div>
+        {/* Arrow Section */}
+        <div className="flex items-center justify-center gap-3 md:flex-col">
+          <Image
+            src={arrows}
+            alt="arrows"
+            width={50}
+            height={50}
+            className="h-[293px] w-full transform object-cover duration-700 ease-out hover:scale-110"
+          />
+        </div>
 
-				<Card {...CardTwo} />
-			</div>
-		</section>
-	);
+        <Card {...CardTwo} />
+      </div>
+    </section>
+  );
 };
 
 export default CardsSection;
