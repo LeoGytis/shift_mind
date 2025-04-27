cf17df7fb33d7e4fa5400695199b64555f597bc5
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-04-27 19:02:12 +0300

    save

--- app/components/layout/TopBar.tsx
+++ app/components/layout/TopBar.tsx
@@ -1,10 +1,54 @@
+"use client";
 import sectionframegreen from "@/app/vectors/sectionframe-green.svg";
 import shiftmind from "@/app/vectors/shiftmind.svg";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const TopBar = () => {
+  const [timeLeft, setTimeLeft] = useState({
+    minutes: 15,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => {
+        if (prevTime.seconds === 0) {
+          if (prevTime.minutes === 0) {
+            clearInterval(timer);
+            return { minutes: 0, seconds: 0 };
+          }
+          return { minutes: prevTime.minutes - 1, seconds: 59 };
+        }
+        return { ...prevTime, seconds: prevTime.seconds - 1 };
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <nav className="">
+      <div className="bg-greendark flex items-center">
+        <div className="mx-auto flex max-w-5xl items-center gap-4 px-4 py-4 text-white">
+          <h3 className="text-wrap !text-white">Time offer expires in</h3>
+          <div className="flex items-center gap-1 md:gap-2">
+            <p className="bg-greenlight flex flex-col items-center rounded-sm px-2 py-1 text-base font-semibold md:px-4 md:py-2 md:text-xl">
+              {timeLeft.minutes.toString().padStart(2, "0")}{" "}
+              <span className="text-xs font-normal md:text-sm">min</span>
+            </p>
+            <span>:</span>
+            <p className="bg-greenlight flex flex-col items-center rounded-sm px-2 py-1 text-base font-semibold md:px-4 md:py-2 md:text-xl">
+              {timeLeft.seconds.toString().padStart(2, "0")}{" "}
+              <span className="text-xs font-normal md:text-sm">sec</span>
+            </p>
+          </div>
+          <button className="background-rainbow flex cursor-pointer rounded-lg px-5 py-3 md:ml-4 md:px-10 md:py-6">
+            GET
+            <span className="hidden md:block">MY PLAN</span>
+          </button>
+        </div>
+      </div>
       <div className="bg-greenlight relative h-24 lg:h-4">
         <Image
           src={shiftmind}
