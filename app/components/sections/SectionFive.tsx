9814cf322d3d2c22628c3fa28f498ea5277b85d7
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-04-27 14:20:20 +0300

    save

---
+++ app/components/sections/SectionFive.tsx
@@ -0,0 +1,59 @@
+import brain from "@/public/images/brain.png";
+import Image from "next/image";
+import { FC } from "react";
+
+interface PlanCardProps {
+  icon: string;
+  description: string;
+}
+
+const planData: PlanCardProps[] = [
+  {
+    icon: "/icons/report.svg",
+    description:
+      "Keep your motivation up by having daily reports of your progress and achievements",
+  },
+  {
+    icon: "/icons/hourglass.svg",
+    description:
+      "Learn new techniques to improve your concentration, effectively handle time, and establish positive daily routines.",
+  },
+  {
+    icon: "/icons/book.svg",
+    description: "Build new habits with our prepared guides",
+  },
+  {
+    icon: "/icons/plan.svg",
+    description:
+      "Have an action plan to break your procrastination cycle and manage procrastination",
+  },
+];
+
+const SectionFive: FC = () => {
+  return (
+    <section className="container mx-auto px-6 py-10 md:px-8">
+      <h1 className="mb-10 text-center md:mb-20">
+        Your Procrastination plan <br /> will help you:
+      </h1>
+      <div className="flex flex-col items-center justify-center gap-10 md:flex-row md:gap-24">
+        <Image
+          src={brain}
+          alt="brain"
+          width={248}
+          height={217}
+          className="order-2 md:order-1 lg:h-[271px] lg:w-[310px]"
+        />
+        <div className="order-1 mt-8 grid grid-cols-1 gap-10 md:order-2 md:gap-15 lg:grid-cols-2">
+          {planData.map((item, index) => (
+            <div key={index} className="flex items-start gap-4 md:max-w-72">
+              <Image src={item.icon} alt="icon" width={48} height={48} />
+              <p className="font-semibold">{item.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SectionFive;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import TopBar from "./components/layout/TopBar";
 import SectionChooseYourPlan from "./components/sections/section-choose-your-plan/SectionChooseYourPlan";
+import SectionFive from "./components/sections/SectionFive";
 import SectionFour from "./components/sections/SectionFour";
 import SectionThree from "./components/sections/SectionThree";
 import SectionYourPersonal from "./components/sections/SectionYourPersonal";
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       <TopBar />
+      <SectionFive />
       <SectionYourPersonal />
       <SectionChooseYourPlan />
       <SectionThree />
